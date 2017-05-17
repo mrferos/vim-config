4fdecf4d4704c358e455906ab45936e072c6dc9b
Andres Galindo <andres.galindo@flexshopper.com>
2017-05-17 10:54:32 -0400

    Updated set up script for osx

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apt-get install ctags
+if [[ "$OSTYPE" == "linux-gnu" ]]
+then
+	# not going to bother detecting distro..
+	apt-get install ctags
+elif [[ "$OSTYPE" == "darwin"* ]] 
+then
+        brew install ctags
+fi
